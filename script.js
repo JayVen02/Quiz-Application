45b722100f37e8e86c43d7abef664e39e6591991
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2024-03-31 17:27:12 +0800

    Corrected

--- script.js
+++ script.js
@@ -1,116 +1,116 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-button')
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-let shuffledQuestions, currectQuestionIndex;
+let shuffledQuestions, currentQuestionIndex;
 let quizScore = 0;
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 
-nextButton.addEventListener('click', () =>{
-    currectQuestionIndex++
-    setnextQuestion()
-})
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
-function startGame(){
-    startButton.classList.add('hide')
-    shuffledQuestions = question.sort(() => Math.random() -0.5)
-    currectQuestionIndex = 0;
-    questionContainerElement,classList.remove('hide')
-    setnextQuestion()
-    quizScore = 0
+function startGame() {
+    startButton.classList.add('hide');
+    shuffledQuestions = question.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+    quizScore = 0;
 }
 
-function setnextQuestion(){
+function setNextQuestion() {
     resetState();
-    showQuestion(shuffledQuestions [currectQuestionIndex])
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
-    question.answer.forEach((answer) =>{
-        const button = document.createElement('button')
+    question.answers.forEach((answer) => {
+        const button = document.createElement('button');
         button.innerText = answer.text;
-        button.classList.add('btn')
-        if(answer.correct){
-            button.dataset.correct = answer.correcy
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 }
 
-function resetState(){
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while(answerButtonsElement.firstChild){
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
 function selectAnswer(e) {
-    const selectButton = e.target
-    const correct = selectButton.dataset.correct
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
 
-    setStatusClass(document.body,correct)
-    Array.from(answerButtonsElement.children).forEach((button)=>{
-        setStatusClass(button,button.dataset.correct)
-})
-    if(shuffledQuestions.length > currectQuestionIndex + 1){
-        nextButton.classList.remove("hide")
-    }else{
-        startButton.innerText = "restart"
-        startButton.classList.remove("hide")
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach((button) => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        startButton.innerText = "Restart";
+        startButton.classList.remove("hide");
     }
-    if(selectedButton.dataset = correct) {
-        quizScore++
+    if (selectedButton.dataset.correct === 'true') {
+        quizScore++;
     }
-    document.getElementById('right-answer').innerText = quizScore
+    document.getElementById('right-answer').innerText = quizScore;
 }
 
-function setStatusClass(element, correct){
-    clearStatusClass(element)
-    if(correct){
-        element.classList.add("correct")
-    }else{
-        element.classList.add("wrong")
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
     }
 }
 
-function clearStatusClass(element){
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
-const question = [
+const questions = [
     {
-        question: 'which one of these is a Javascript framework?',
+        question: 'Which one of these is a JavaScript framework?',
         answers: [
-            {text: 'Python', correct: false},
-            {text: 'Django', correct: false},
-            {text: 'React', correct: true},
-            {text: 'Eclipse', correct: false}
+            { text: 'Python', correct: false },
+            { text: 'Django', correct: false },
+            { text: 'React', correct: true },
+            { text: 'Eclipse', correct: false }
         ],
     },
     {
         question: 'Who is the Sun?',
         answers: [
-            {text: 'Universe', correct: false},
-            {text: 'Planet', correct: false},
-            {text: 'Joven', correct: true},
-            {text: 'Araw', correct: false}
+            { text: 'Universe', correct: false },
+            { text: 'Planet', correct: false },
+            { text: 'Joven', correct: true },
+            { text: 'Araw', correct: false }
         ],
-    },    
+    },
     {
         question: '4 + 10',
         answers: [
-            {text: 'I miss You', correct: false},
-            {text: 'I love you', correct: false},
-            {text: 'Hi', correct: false},
-            {text: '14', correct: true}
+            { text: 'I miss You', correct: false },
+            { text: 'I love you', correct: false },
+            { text: 'Hi', correct: false },
+            { text: '14', correct: true }
         ],
     },
-]
+];
